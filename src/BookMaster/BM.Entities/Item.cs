79bc6cae13c73a1f14b634d93b355ddc47d3eb82
LWolf76 <developer13@magicwithmark.com>
2014-02-10 08:28:40 -0600

    Adjust Entities for all media

---
+++ src/BookMaster/BM.Entities/Item.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BM.Entities
+{
+    class Item
+    {
+        public Int32 ItemId { get; set; }
+        public Int32 Type { get; set; }
+        public String Title { get; set; }
+        public String Author { get; set; }
+        public String PersonalAuthor { get; set; }
+        public String ISBN { get; set; }
+        public String Publisher { get; set; }
+        public Int32 Published { get; set; }
+        public String Edition { get; set; }
+        public String PublicationInformation { get; set; }
+        public Int32 Pages { get; set; }
+        public Int32 Subject { get; set; }
+        public String Location { get; set; }
+        public String Summary { get; set; }
+    }
+}
---
+++ src/BookMaster/BM.Entities/ItemContent.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BM.Entities
+{
+    class ItemContent
+    {
+        public Int32 ItemContentId { get; set; }
+        public String Description { get; set; }
+        public Int32 Subject { get; set; }
+
+        public Int32 ItemId { get; set; }
+    }
+}
