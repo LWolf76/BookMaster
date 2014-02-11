a7b3c6eed9ca9cfe0854d136ae35948a70f13da1
LWolf76 <developer13@magicwithmark.com>
2014-02-11 17:51:04 -0600

    Updated UI

--- src/BookMaster/BM.Web/Controllers/HomeController.cs
+++ src/BookMaster/BM.Web/Controllers/HomeController.cs
@@ -1,32 +1,56 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Web;
 using System.Web.Mvc;
+using BM.Web.Models;
 
 namespace BM.Web.Controllers
 {
     public class HomeController : Controller
     {
+        
+        BookMasterDb _db = new BookMasterDb();
+        
         public ActionResult Index()
         {
-            ViewBag.Message = "ASP.NET MVC application.";
 
-            return View();
+            var model = _db.Items.ToList();
+
+            ViewBag.Message = " When a Simple Search is Just not Enough!";
+
+            return View(model);
+        }
+
+        public ActionResult Edit(int id)
+        {
+            var model = (from i in _db.Items where i.ItemId == id select i).Single();
+            ViewBag.Message = "Edit Page";
+            return View(model);
         }
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your app description page.";
+            ViewBag.Message = "About Us";
 
             return View();
         }
 
         public ActionResult Contact()
         {
-            ViewBag.Message = "Your contact page.";
+            ViewBag.Message = "Contact Us";
 
             return View();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (_db != null)
+            {
+                _db.Dispose();
+            }
+            base.Dispose(disposing);
         }
     }
 }
