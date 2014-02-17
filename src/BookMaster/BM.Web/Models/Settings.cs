a51243939b8571ad0ee936c6043f380b7f74454a
LWolf76 <developer13@magicwithmark.com>
2014-02-17 17:18:07 -0600

    Add Settings To the project for Custom Name

---
+++ src/BookMaster/BM.Web/Models/Settings.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BM.Web.Models
+{
+    public class Settings
+    {
+        public Int32 SettingId { get; set; }
+        public String DatabaseName { get; set; }
+    }
+}
