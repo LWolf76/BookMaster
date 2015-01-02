7cf32c9bb39a532c4ae6641b0312e34fd31a19c9
LWolf76 <developer13@magicwithmark.com>
2015-01-02 17:31:50 -0600

    Changed Seed

--- src/BookMaster/BookMaster/DAL/BookMasterInitializer.cs
+++ src/BookMaster/BookMaster/DAL/BookMasterInitializer.cs
@@ -9,10 +9,48 @@ using BookMaster.Models;
 
 namespace BookMaster.DAL
 {
-    public class BookMasterInitializer : System.Data.Entity.DropCreateDatabaseIfModelChanges<BookMasterContext> //DropCreateDatabaseAlways<BookMasterContext>
+    public class BookMasterInitializer : System.Data.Entity.DropCreateDatabaseIfModelChanges<BookMasterContext> // DropCreateDatabaseAlways 
     {
         protected override void Seed(BookMasterContext context)
         {
+
+            var subjects = new List<Subject>
+            {
+                new Subject
+                {
+                    Description = "Coins"
+                },
+                new Subject()
+                {
+                    Description = "Cards"
+                },
+                new Subject()
+                {
+                    Description = "Wand"
+                },
+                new Subject()
+                {
+                    Description = "Balls"
+                },
+                new Subject()
+                {
+                    Description = "Silks"
+                },
+                new Subject()
+                {
+                    Description = "Match"
+                },
+                new Subject()
+                {
+                    Description = "Rubber Band"
+                },
+                new Subject()
+                {
+                    Description = "Paper Clip"
+                }
+            };
+            subjects.ForEach(s => context.Subjects.Add(s));
+
             var items = new List<Item>
             {
                 new Item
@@ -33,40 +71,48 @@ namespace BookMaster.DAL
                 },
                 new Item
                 {
-                    ItemType = 0,
-                    Title = "Book Two Title",
-                    Author = "B2 Author",
-                    Edition = "B2 Edition",
+                    ItemType = ItemType.Book,
+                    Title = "The Expert at the Card Table",
+                    Author = "S. W. Erdnase",
+                    Publisher = "Unknown",
+                    Published = 1902,
+                    Edition = "Hardcover",
                     ISBN = "B2 ISBN",
                     Location = "B2 Location",
-                    Pages = 250
+                    Pages = 205
                 },
                 new Item
                 {
-                    ItemType = 0,
-                    Title = "Book Three Title",
-                    Author = "B3 Author",
-                    Edition = "B3 Edition",
+                    ItemType = ItemType.Book,
+                    Title = "The Art of Astonishment - Book 1",
+                    Author = "Paul Harris & Eric Mead",
+                    Publisher = "A-1 MultiMedia",
+                    Published = 1996,
+                    Edition = "Hardcover",
                     ISBN = "B3 ISBN",
                     Location = "B3 Location",
-                    Pages = 350
+                    Pages = 310
                 },
                 new Item
                 {
                     ItemType = 0,
-                    Title = "Book Four Title",
-                    Author = "B4 Author",
-                    Edition = "B4 Edition",
+                    Title = "The Magic of Michael Ammar",
+                    Author = "Michael Ammar",
+                    Publisher = "L&L Publishing",
+                    Published = 1991,
+                    Edition = "Hardcover",
                     ISBN = "B4 ISBN",
                     Location = "B4 Location",
-                    Pages = 450
+                    Pages = 306
                 },
                 new Item
                 {
                     ItemType = 0,
                     Title = "Best of Friends",
                     Author = "Harry Lorayne",
-                    Edition = "B4 Edition",
+                    Publisher = "Harry Lorayne, Inc.",
+                    Published = 1982,
+                    Edition = "Hardcover",
                     ISBN = "B4 ISBN",
                     Location = "B4 Location",
                     Pages = 559
@@ -74,8 +120,34 @@ namespace BookMaster.DAL
                 new Item
                 {
                     ItemType = 0,
+                    Title = "Rim Shots",
+                    Author = "Harry Lorayne",
+                    Publisher = "Harry Lorayne, Inc.",
+                    Published = 1973,
+                    Edition = "Hardcover",
+                    ISBN = "B4 ISBN",
+                    Location = "B4 Location",
+                    Pages = 148
+                },
+                new Item
+                {
+                    ItemType = 0,
+                    Title = "The Cardician",
+                    Author = "Ed Marlo",
+                    Publisher = "Magic, Inc.",
+                    Published = 1953,
+                    Edition = "Spiralbound",
+                    ISBN = "B4 ISBN",
+                    Location = "B4 Location",
+                    Pages = 199
+                },
+                new Item
+                {
+                    ItemType = 0,
                     Title = "Tarbell Course in Magic - Volume 1",
                     Author = "Harlan Tarbell",
+                    Publisher = "Robbins",
+                    Published = 1941,
                     Edition = "B1 Edition",
                     ISBN = "B1 ISBN",
                     Location = "B1 Location",
@@ -86,50 +158,60 @@ namespace BookMaster.DAL
                     ItemType = 0,
                     Title = "Tarbell Course in Magic - Volume 2",
                     Author = "Harlan Tarbell",
+                    Publisher = "Robbins",
+                    Published = 1942,
                     Edition = "B2 Edition",
                     ISBN = "B2 ISBN",
                     Location = "B2 Location",
-                    Pages = 413
+                    Pages = 416
                 },
                 new Item
                 {
                     ItemType = 0,
                     Title = "Tarbell Course in Magic - Volume 3",
                     Author = "Harlan Tarbell",
+                    Publisher = "Robbins",
+                    Published = 1943,
                     Edition = "B2 Edition",
                     ISBN = "B2 ISBN",
                     Location = "B2 Location",
-                    Pages = 413
+                    Pages = 416
                 },
                 new Item
                 {
                     ItemType = 0,
                     Title = "Tarbell Course in Magic - Volume 4",
                     Author = "Harlan Tarbell",
+                    Publisher = "Robbins",
+                    Published = 1945,
                     Edition = "B2 Edition",
                     ISBN = "B2 ISBN",
                     Location = "B2 Location",
-                    Pages = 413
+                    Pages = 416
                 },
                 new Item
                 {
                     ItemType = 0,
                     Title = "Tarbell Course in Magic - Volume 5",
                     Author = "Harlan Tarbell",
+                    Publisher = "Robbins",
+                    Published = 1948,
                     Edition = "B2 Edition",
                     ISBN = "B2 ISBN",
                     Location = "B2 Location",
-                    Pages = 413
+                    Pages = 417
                 },
                 new Item
                 {
                     ItemType = 0,
                     Title = "Tarbell Course in Magic - Volume 6",
                     Author = "Harlan Tarbell",
+                    Publisher = "Robbins",
+                    Published = 1954,
                     Edition = "B2 Edition",
                     ISBN = "B2 ISBN",
                     Location = "B2 Location",
-                    Pages = 413
+                    Pages = 409
                 }
             };
             items.ForEach(s => context.Items.Add(s));
