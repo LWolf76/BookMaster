1b3dec33dfacd49af3a3143c72e695a56c65d981
LWolf76 <developer13@magicwithmark.com>
2015-01-02 12:13:25 -0600

    Removed Personal Author

--- src/BookMaster/BookMaster/Models/Item.cs
+++ src/BookMaster/BookMaster/Models/Item.cs
@@ -14,13 +14,15 @@ namespace BookMaster.Models
     public class Item
     {
         public Int32 ItemId { get; set; }
+
+        [Display(Name = "Type")]
+        [Required(ErrorMessage = "Type Required")]
         public ItemType? ItemType { get; set; }
 
         [Required(ErrorMessage = "Title Required")]
         public String Title { get; set; }
 
         public String Author { get; set; }
-        public String PersonalAuthor { get; set; }
         public String ISBN { get; set; }
         public String Publisher { get; set; }
         public Int32 Published { get; set; }
